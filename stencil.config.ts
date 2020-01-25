348c89886e6c5afa4c8ee95b1010ac518dc6dce8
Ryosuke <ryosuke.san.hana@gmail.com>
2020-01-24 16:16:13 -0800

    :heavy_plus_sign: sass enabled

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,18 +1,20 @@
-import { Config } from '@stencil/core';
+import { Config } from "@stencil/core";
+import { sass } from "@stencil/sass";
 
 export const config: Config = {
-  namespace: 'sui-web-components',
+  namespace: "sui-web-components",
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader'
+      type: "dist",
+      esmLoaderPath: "../loader"
     },
     {
-      type: 'docs-readme'
+      type: "docs-readme"
     },
     {
-      type: 'www',
+      type: "www",
       serviceWorker: null // disable service workers
     }
-  ]
+  ],
+  plugins: [sass()]
 };
